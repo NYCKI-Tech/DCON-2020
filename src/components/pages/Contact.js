597f6a0b0cbd0d36b9f11a4209a4fdbfecaf3574
Brendon Nguyen <btn28@cornell.edu>
2021-01-24 20:53:35 -0500

    added mailto links on contact and updated text on involvements page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,9 +3,12 @@ import React from 'react';
 const Contact = () => {
     return(<div className="pageContainer">
         <p style={{width: '50%', margin: 'auto', 'text-align':'left'}}>
-            <h1 style={{'text-align':'center'}}>Email us!</h1>             
-            Feel free to reach out to Conferences and Conventions chairs Joan Cheng and Elizabeth Dosman if you have any questions! 
-            Their emails are: joan.cheng@nycirclek.org & elizabeth.dosman@nycirclek.org
+            <h1 style={{'text-align':'center'}}>Email Us!</h1>
+            Please reach out to the Conferences and Conventions Chairs with any questions, comments, or concerns.
+            <br></br>
+            Joan Cheng: <a href="mailto:joan.cheng@nycirclek.org">joan.cheng@nycirclek.org </a>
+            <br></br>
+            Elizabeth Dosman: <a href="mailto:elizabeth.dosman@nycirclek.org">elizabeth.dosman@nycirclek.org</a>
         </p>
 
     </div>);
--- src/components/pages/Involvement.js
+++ src/components/pages/Involvement.js
@@ -3,18 +3,24 @@ import React from 'react';
 const Involvement = () => {
     return(<div className="pageContainer">
         <p style={{width: '50%', margin: 'auto', 'text-align':'left'}}>
-            <h1 style={{'text-align':'center'}}>Team Leader Application</h1>             
-            A Team Leader is one of the most important parts of any convention, NYS being no different. We're looking for people who are 
-            dedicated, passionate, and enthusiastic to help lead attendees through the various aspects of NYS. Whether it be service projects,
-            fellowship activities, or just checking up on your team, you'll be tasked with ensuring that attendees have a memorable NYS. Your 
-            impact on your team cannot go understated, and we hope that you'll choose to apply!
+            <h1 style={{'text-align':'center'}}>Team Leader Application</h1>
+            A Team Leader is one of the most important parts of any convention, DCON being no different. We're looking for people who are
+            dedicated, passionate, and enthusiastic to help lead attendees through the various aspects of  DCON. Whether it be service projects,
+            fellowship activities, or just checking up on your team, you'll be tasked with ensuring that attendees have a memorable DCON. Your
+            impact on your team cannot go understated, and we can't wait for you to apply!
         </p>
         <p style={{'margin-top': '1.2%'}}>
            <button onClick={(()=>{window.location.href="https://forms.gle/13MDzMx999cjWsXVA"})} >Apply here!</button>
         </p>
-        <p>Dress Your President Form: https://docs.google.com/forms/d/1qIhBMdvH1VTTVQ6p-LNSP6EzgJleY6sMAtmGpPahV0A/edit</p>
-        <p>Workshop Application: email erick.morocho@nycirclek.org</p>
-        
+        <p>
+          <h1 style={{'text-align':'center'}}>Lip Sync Battle Signup</h1>
+          Coming soon
+        </p>
+        <p>
+          <h1 style={{'text-align':'center'}}>Workshop Application</h1>
+          Coming soon
+        </p>
+
     </div>);
 }
 
